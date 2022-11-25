419fc3f3c688faa42ea871b3090e7bb5603ae3c9
Andres <anmejiav@unal.edu.co>
2022-11-25 18:46:32 -0500

    fin del ejercicio7

---
+++ ejercicio7/index.js
@@ -0,0 +1,26 @@
+var receta={};
+receta.nombre="sandwich";
+receta.ingredientes=[];
+var ingre={
+    nombre: "pan",
+    cantidad:2,
+};
+receta.ingredientes=[ingre];
+var ingre2={
+    nombre: "Queso",
+    cantidad:1,
+};
+receta.ingredientes=[ingre,ingre2];
+
+//console.log(receta.ingredientes[0].nombre);
+//console.log(receta.ingredientes[0].cantidad+receta.ingredientes[1].cantidad);
+
+//
+function cantidad(num){
+let resultado=0;
+for(i=0;i<num;i++){
+    resultado=receta.ingredientes[i].cantidad+resultado;
+}
+return resultado;
+}
+console.log(cantidad(2));
